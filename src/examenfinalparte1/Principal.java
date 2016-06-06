f4aaefda12885d8dd4f0284bce7c2bc23e3acf5e
aparracorbacho <aparracorbacho@atanasoff20.danielcastelao.org>
2016-06-06 08:48:15 +0200

    Explicamos todos los metodos de la clase Principal

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -3,20 +3,35 @@ package examenfinalparte1;
 public class Principal {
 
     int i;
-
+    
+    /*
+    * Este metodo mete en la variable i la suma de los dos numeros que se le pasan
+    */
     public Principal(int j, int m) {
-        i = met(j, m);
+        i = suma(j, m);
     }
-
+    
+    /*
+    * Este metodo suma el numero que se le pasa consigo mismo y lo mete en i
+    */
+    
     public Principal(int j) {
-        i = met(j, j);
+        i = suma(j, j);
     }
-
+    
+    /*
+    * Este metodo coloca la variable i en -1
+    */
+    
     public Principal() {
         i = -1;
     }
 
-    public int met(int x, int x1) {
-        return x + x1;
+    /*
+    * Renombramos el metodo que suma variables
+    * @return devuelve la suma de las dos variables
+    */
+    public int suma(int numero1, int numero2) {
+        return numero1 + numero2;
     }
 }
